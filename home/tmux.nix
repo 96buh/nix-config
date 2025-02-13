b9e54d17c7a92c1351e2f3bad30e9d426ec77496
Homer <j29823678@gmail.com>
2025-02-14 01:43:00 +0800

    config tmux

--- home/tmux.nix
+++ home/tmux.nix
@@ -3,4 +3,19 @@
 {
     enable = true;
     baseIndex = 1;
+    shell = "${pkgs.zsh}/bin/zsh";
+    terminal = "xterm-256color";
+
+    plugins = with pkgs.tmuxPlugins; [
+        gruvbox
+        sensible
+        vim-tmux-navigator
+    ];
+    extraConfig = ''
+        set -as terminal-features ",xterm-256color:RGB"
+        set -g mouse on
+        set -g default-command ${pkgs.zsh}/bin/zsh
+
+        set-option -g status-position top
+    '';
 }
