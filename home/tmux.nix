e6a002202258aa55f5123df131cecaab91bd1fe7
Homer <j29823678@gmail.com>
2025-08-22 12:15:04 +0800

    tmux: simplify appearance to minimal style

--- home/tmux.nix
+++ home/tmux.nix
@@ -8,7 +8,6 @@
     prefix = "C-b";
 
     plugins = with pkgs.tmuxPlugins; [
-        gruvbox
         sensible
         vim-tmux-navigator
     ];
@@ -16,6 +15,20 @@
         set -as terminal-features ",xterm-256color:RGB"
         set -g mouse on
         set -g default-command ${pkgs.zsh}/bin/zsh
-        set -g @tmux-gruvbox-right-status-x '%m/%d/%Y' # e.g.: 01/31/2024
+
+        set -g status-position top
+        set -g status-justify centre
+
+        set -g status-right ""
+
+        set -g status-left "#S "
+
+        set -g status-style "bg=default,fg=#a89984"
+
+        set -g window-status-format "#[fg=#a89984]#I:#W"
+        set -g window-status-current-format "#[fg=#fabd2f,bold]#I:#W"
+
+        set -g message-style "bg=#3c3836,fg=#ebdbb2"
+        set -g message-command-style "bg=#3c3836,fg=#ebdbb2"
     '';
 }
